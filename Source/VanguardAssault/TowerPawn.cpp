ff78bf10c640504f16c6d01bd8b22d4be8580d15
Serhii Viter <vitersergii@gmail.com>
2024-05-20 22:24:14 +0300

    Added task 5

--- Source/VanguardAssault/TowerPawn.cpp
+++ Source/VanguardAssault/TowerPawn.cpp
@@ -1,12 +1,59 @@
 #include "TowerPawn.h"
+#include "GameFramework/Actor.h"
+#include "TimerManager.h"
+#include "Engine/World.h"
 
 ATowerPawn::ATowerPawn()
 {
-
+    DetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphere"));
+    DetectionSphere->SetupAttachment(RootComponent);
+    DetectionSphere->SetSphereRadius(2000.0f);
+    DetectionSphere->OnComponentBeginOverlap.AddDynamic(this, &ATowerPawn::OnOverlapBegin);
+    DetectionSphere->OnComponentEndOverlap.AddDynamic(this, &ATowerPawn::OnOverlapEnd);
 }
 
 void ATowerPawn::BeginPlay()
 {
     Super::BeginPlay();
+}
 
+void ATowerPawn::OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+    if (ATankPawn* Player = Cast<ATankPawn>(OtherActor))
+    {
+        TargetActor = Player;
+        GetWorldTimerManager().SetTimer(UpdateTimerHandle, this, &ATowerPawn::UpdateTurretRotation, UpdateInterval, true);
+        GetWorldTimerManager().SetTimer(FireTimerHandle, this, &ATowerPawn::FireAtPlayer, FireInterval, true);
+    }
+}
+
+void ATowerPawn::OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+    UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+    if (TargetActor && OtherActor == TargetActor)
+    {
+        GetWorldTimerManager().ClearTimer(UpdateTimerHandle);
+        GetWorldTimerManager().ClearTimer(FireTimerHandle);
+        TargetActor = nullptr;
+    }
+}
+
+void ATowerPawn::UpdateTurretRotation()
+{
+    if (TargetActor)
+    {
+        FVector Direction = TargetActor->GetActorLocation() - TurretMesh->GetComponentLocation();
+        Direction.Z = 0;
+        Direction = Direction.RotateAngleAxis(-90.0f, FVector(0.0f, 0.0f, 1.0f));
+        RotateTurret(Direction);
+    }
+}
+
+void ATowerPawn::FireAtPlayer()
+{
+    if (TargetActor)
+    {
+        UE_LOG(LogTemp, Warning, TEXT("Fire!"));
+    }
 }
--- Source/VanguardAssault/TowerPawn.h
+++ Source/VanguardAssault/TowerPawn.h
@@ -2,13 +2,15 @@
 
 #include "CoreMinimal.h"
 #include "TurretPawn.h"
+#include "Components/SphereComponent.h"
+#include "TankPawn.h"
 #include "TowerPawn.generated.h"
 
 
 UCLASS()
 class VANGUARDASSAULT_API ATowerPawn : public ATurretPawn
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
     ATowerPawn();
@@ -16,4 +18,30 @@ public:
 protected:
     virtual void BeginPlay() override;
 
+private:
+    UPROPERTY(VisibleAnywhere, Category = "Components")
+    USphereComponent* DetectionSphere;
+
+    UPROPERTY(EditAnywhere, Category = "Combat")
+    float FireInterval = 5.0f;
+
+    UPROPERTY(EditAnywhere, Category = "Combat")
+    float UpdateInterval = 0.01f;
+
+    UFUNCTION()
+    void OnOverlapBegin(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+    UFUNCTION()
+    void OnOverlapEnd(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+        UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+    UFUNCTION(BlueprintCallable, Category = "Combat")
+    void FireAtPlayer();
+
+    void UpdateTurretRotation();
+
+    FTimerHandle UpdateTimerHandle;
+    FTimerHandle FireTimerHandle;
+    AActor* TargetActor;
 };
